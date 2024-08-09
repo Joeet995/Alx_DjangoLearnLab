a518501916c8a21b68d1cdde9ee5cb8fa83e7ee0
youssef Tarek <yousef.tarek095@gmail.com>
2024-08-09 21:02:32 +0300

    last

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/models.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/models.py
@@ -2,15 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Book:
-    def __init__(self, title : str, author : str, publication_year: int):
-        self.title = title
-        self.author = author
-        self.publication_year = publication_year
+class Book(models.Model):
 
-        if len(title) > 200:
-            raise Exception("title given is more then 200 characters")
-        
-        if len(author) > 100:
-            raise Exception("author name is more than 100 characters")
-        
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+
