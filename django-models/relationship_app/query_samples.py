67f493cea44311d3a4254ad30df8ac882271614a
youssef Tarek <yousef.tarek095@gmail.com>
2024-08-15 18:56:31 +0300

    lasj

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from .models import Author, Book, Library, Librarian
 
 Book.objects.filter(author="george Orwell")
 
-Library.objects.get(name="library_name")
+Library.objects.get(name=library_name)
 books.all()
 
 Librarian.objects.grt(library="stans")
