769e24fdb91f451c5611b64a8e0635ea1edcac43
youssef Tarek <yousef.tarek095@gmail.com>
2024-08-15 18:50:36 +0300

    tasks

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,6 +3,6 @@ from .models import Author, Book, Library, Librarian
 
 Book.objects.filter(author="george Orwell")
 
-Library.objects.get(name="stans")
+Library.objects.get(name="library_name")
 
 Librarian.objects.grt(library="stans")
--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,6 +3,7 @@ from .models import Author, Book, Library, Librarian
 
 Book.objects.filter(author="george Orwell")
 
-Library.objects.get(name="stans")
+Library.objects.get(name="library_name")
+
 
 Librarian.objects.grt(library="stans")
