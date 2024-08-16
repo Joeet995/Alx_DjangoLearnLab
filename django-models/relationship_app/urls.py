b564a30b693c3edb5b596ad7d953c5cc32d03a89
youssef Tarek <yousef.tarek095@gmail.com>
2024-08-16 20:58:06 +0300

    task 2

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import list_books
+from .views import register, list_books, LoginView
 from .views import LibraryDetailView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('list_books', list_books, name='list_books'),
     path('library_detail', LibraryDetailView.as_view(), name='library_detail'),
+    path("register", register,name="register"),
+    path("login", LoginView.as_view(),name="login"),
+    path("logout", LogoutView.as_view(),name="logout"),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def list_books(request):
@@ -13,3 +15,16 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("Login")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form":form})
+
+class LoginView(LoginView):
+    template_name = "login.html"
