d270a57994d1af01a57793d1c1795c863bf06f9f
youssef Tarek <yousef.tarek095@gmail.com>
2024-08-19 01:36:15 +0300

    task 3

--- django-models/relationship_app/librarian_view.py
+++ django-models/relationship_app/librarian_view.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, login_required
 from django.shortcuts import render
 from . import models
+
 
 def is_librarian(user):
     return  user.UserProfile.role == 'Librarian'
@@ -9,7 +10,7 @@ def is_librarian(user):
 # def librarian_view(request):
 #     return render(request, template_name="relationship_app/librarian_view.html")
 
-
+@login_required
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'profiles/librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
