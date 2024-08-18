c85c694ecc64a6481ff160ea2aea9ccc8f5c3ec1
youssef Tarek <yousef.tarek095@gmail.com>
2024-08-18 20:29:51 +0300

    task 33

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("login", LoginView.as_view(template_name="login.html"),name="login"),
     path("logout", LogoutView.as_view(template_name="logout.html"),name="logout"),
     path('admins', views.admin_view, name='admins'),
-    path('librarians', views.librarian_view, name='librarians'),
+    path('librarian', views.librarian_view, name='librarian'),
 ]
